e8a1622aaddb41de62aa59cabf0cfbb564834a0c
Max zhang <373884384@qq.com>
2019-11-29 14:21:44 +0800

    修改成为井字棋

--- react/one-app/src/index.js
+++ react/one-app/src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" onClick={() => this.props.onClick()}>
-            {this.props.value}
+        <button className="square" onClick={() => props.onClick()} >
+            {props.value}
         </button>
     );
 }
@@ -16,9 +16,10 @@ class Board extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            squares: Array(this.props.row * this.props.col).fill(null)
+            squares: Array(this.props.row * this.props.col)
             , xNext: true
         }
+        console.log(this.state)
     }
 
     renderSquare(i) {
@@ -26,7 +27,10 @@ class Board extends React.Component {
     }
 
     handleClick(i) {
-        const squares = this.state.squares.slice();
+        const squares = this.state.squares.slice(); console.log(i)
+        if (calWinner(squares)) {
+            return;
+        }
         if (!squares[i]) {
             squares[i] = this.state.xNext ? 'X' : 'O';
             this.setState({ squares: squares, xNext: !this.state.xNext });
@@ -34,18 +38,24 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xNext ? 'X' : 'O');
-        var h = [], c = 0;
-        let row = (r) => {
+        let status;
+        let winner = calWinner(this.state.squares)
+        if (winner) {
+            status = 'Winner is ' + (winner);
+        } else {
+            status = 'Next player: ' + (this.state.xNext ? 'X' : 'O')
+        }
+        var h = [];
+        let row = (x) => {
             var _h = [];
-            for (var j = 1; j <= r; j++) {
-                _h.push(this.renderSquare(c)); c++;
+            for (var j = 0; j < this.props.col; j++) {
+                _h.push(this.renderSquare(x * 10 + j));
             }
             return _h
         }
 
-        for (var i = 1; i <= this.props.row; i++) {
-            h.push(<div className="row">{row(this.props.col)}</div>)
+        for (var i = 0; i < this.props.row; i++) {
+            h.push(<div className="row">{row(i)}</div>)
         }
 
         return (
@@ -56,13 +66,32 @@ class Board extends React.Component {
         );
     }
 }
+function calWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [10, 11, 12],
+        [20, 21, 22],
+        [0, 10, 20],
+        [11, 11, 21],
+        [2, 12, 22],
+        [0, 11, 22],
+        [2, 11, 20],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 class Game extends React.Component {
     render() {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board row="10" col="10" />
+                    <Board row={3} col={3} />
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
